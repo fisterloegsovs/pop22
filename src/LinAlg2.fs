453c08ea746199ea412f3c8cc0989e0a5786c25e
Jon Sporring <sporring@di.ku.dk>
2015-07-21 07:04:18 +0000

    cleanup

--- src/LinAlg2.fs
+++ src/LinAlg2.fs
@@ -1,35 +1,54 @@
 //module LinAlg
-type Vector = V of float * (float list)
-let VectorMap f (V(x,xs)) = V(f x, List.map f xs)
-let VectorFold f e (V(x,xs)) = List.fold f (f e x) xs
-let VectorLength = VectorFold (fun e x -> e+1) 0
-let listLength = List.fold (fun e x -> e+1) 0
-let coord (V(x,xs)) = x::xs
+type Vector = V of float list
 let rec zip = function
-  | ([],_) -> []
+  | ([],_)
   | (_,[]) -> []
   | (x::xs,y::ys) -> (x,y)::(zip (xs, ys))
-let make  = function
-  | (x::xs) -> V(x,xs)
-  | _ -> V(0.0,[])
+let VectorLength (V(xs)) = List.length xs
 type Vector with
-  static member ( ~- ) (V(x,xs)) = VectorMap (fun x -> -x) (V(x,xs))
-  static member ( + ) (V(x1,xs1),V(x2,xs2)) =
-    if listLength xs1 = listLength xs2 then
-      let zipped = (x1,x2)::(zip (xs1,xs2))
+  static member ( ~- ) (V(xs)) = V(List.map (fun x -> -x) (xs))
+  static member ( + ) (V(xs),V(ys)) =
+    if VectorLength (V(xs)) <> VectorLength (V(ys)) then
+      failwith "Sum of 2 vectors of unequal lengths is undefined"
+    else
+      let zipped = zip (xs, ys)
       let added = List.map (fun (a,b) -> a+b) zipped
-      Some (make added)
-    else None
-let print (V(x,xs)) = printf "[ %f" x; List.iter (printf ", %f") xs; printfn "]"
+      V(added)
+  static member ( - ) (V1,V2) = V1 + - V2
+  static member ( * )  (a, V(xs)) = V(List.map (fun x -> a*x ) xs)
+  static member ( * )  (V(xs), V(ys)) =
+    if VectorLength (V(xs)) <> VectorLength (V(ys)) then
+      failwith "Product of 2 vectors of unequal lengths is undefined"
+    else
+      let zipped = zip (xs, ys)
+      List.fold (fun sum (a,b) -> sum + a*b) 0.0 zipped
+let norm (V(xs)) = sqrt(V(xs) * V(xs))
+let rec VectorPrint = function
+  | (V(x::xs)) -> printf "[ %f" x; List.iter (printf ", %f") xs; printfn " ]"
+  | V([]) -> printfn "[]"
 
-let v = make([1.0..5.0])
-print v
-printfn "v has length: %d" (VectorLength v)
+let v0 = V([1.0..5.0])
+printfn "v0 in R^%d = " (VectorLength v0)
+VectorPrint v0
 
-let w = -v
-print w
-printfn "w has length: %d" (VectorLength w)
+let v1 = -v0
+printfn "-v0 = v1 in R^%d = " (VectorLength v1)
+VectorPrint v1
 
-let u = Option.get <| v+make([0.0..0.1..0.4])
-print u
-printfn "u has length: %d" (VectorLength u)
+let v2 = v0+V([0.0..0.1..0.4])
+printfn "V0 + V([0.0..0.1..0.4]) = v2 in R^%d = " (VectorLength v2)
+VectorPrint v2
+
+let v3 = v0-V([0.0..0.1..0.4])
+printfn "v0-V([0.0..0.1..0.4]) = v3 in R^%d = " (VectorLength v3)
+VectorPrint v3
+
+let v4 = 3.0*v3
+printfn "3.0*v3 = v4 in R^%d = " (VectorLength v4)
+VectorPrint v4
+
+let v5 = v3*v4
+printfn "v3*v4 = v5 = %f" v5
+
+let v6 = norm v3
+printfn "norm v3 = v6 = %f" v6
