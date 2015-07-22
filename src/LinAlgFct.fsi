9d856b8dc459563e80b42ab60b968db5f1f5c557
Jon Sporring <sporring@di.ku.dk>
2015-07-22 20:21:12 +0000

    better coding style

--- src/LinAlgFct.fsi
+++ src/LinAlgFct.fsi
@@ -1,14 +1,12 @@
 module LinAlgFct
 [<Sealed>]
 type Vector =
-  | V of float list
-  with
-    static member ( + ) : Vector * Vector -> Vector
-    static member ( * ) : a:float * Vector -> Vector
-    static member ( * ) : Vector * Vector -> float
-    static member ( - ) : Vector * Vector -> Vector
-    static member ( ~- ) : Vector -> Vector
-  end
+  static member ( + ) : Vector * Vector -> Vector
+  static member ( * ) : a:float * Vector -> Vector
+  static member ( * ) : Vector * Vector -> float
+  static member ( - ) : Vector * Vector -> Vector
+  static member ( ~- ) : Vector -> Vector
+val Vector : float list -> Vector
 val length : Vector -> int
 val VectorGetSlice : Vector * rowStart:int * rowFinish:int -> Vector
 val VectorNorm : Vector -> float
@@ -17,23 +15,22 @@ val VectorToString : Vector -> string
 val VectorOne : int -> Vector
 val VectorRandom : int -> Vector
     
+[<Sealed>]
 type Matrix =
-  | M of float list list
-  with
-    static member ( + ) : Matrix * Matrix -> Matrix
-    static member ( * ) : a:float * Matrix -> Matrix
-    static member ( * ) : Matrix * Matrix -> Matrix
-    static member ( * ) : Matrix * Vector -> Matrix
-    static member ( * ) : Vector * Matrix -> Matrix
-    static member ( - ) : M1:int * M2:int -> int
-    static member ( ~- ) : Matrix -> Matrix
-  end
+  static member ( + ) : Matrix * Matrix -> Matrix
+  static member ( * ) : a:float * Matrix -> Matrix
+  static member ( * ) : Matrix * Matrix -> Matrix
+  static member ( * ) : Matrix * Vector -> Matrix
+  static member ( * ) : Vector * Matrix -> Matrix
+  static member ( - ) : Matrix * Matrix -> Matrix
+  static member ( ~- ) : Matrix -> Matrix
+val Matrix: float list list -> Matrix
 val rows : Matrix -> int
 val cols : Matrix -> int
 val transpose : Matrix -> Matrix
 val VectorToMatrix : Vector -> Matrix
 val MatrixToVector : Matrix -> Vector
-val MatrixGetSlice : Matrix * rowStart:int * rowFinish:int * colStart:int * colFinish:int -> Matrix
+val MatrixGetSlice : Matrix * int * int * int * int -> Matrix
 val RowConcat : Matrix * Matrix -> Matrix
 val ColConcat : Matrix * Matrix -> Matrix
 val Minor : Matrix * int * int -> Matrix
--- src/testLinAlgFct.fsx
+++ src/testLinAlgFct.fsx
@@ -1,16 +1,16 @@
 open LinAlgFct
 
-let v0 = V([1.0..5.0])
+let v0 = Vector( [1.0 .. 5.0] )
 printfn "v0 in R^%d =\n%s" (length v0) (VectorToString v0)
 
-let v1 = -v0
-printfn "-v0 = v1 in R^%d =\n%s" (length v1) (VectorToString v1)
+let v1 = - v0
+printfn " - v0 = v1 in R^%d =\n%s" (length v1) (VectorToString v1)
 
-let v2 = v0+V([0.0..0.1..0.4])
-printfn "V0 + V([0.0..0.1..0.4]) = v2 in R^%d =\n%s" (length v2) (VectorToString v2)
+let v2 = v0 + Vector( [0.0 .. 0.1 .. 0.4] )
+printfn "V0 + Vector( [0.0 .. 0.1 .. 0.4] ) = v2 in R^%d =\n%s" (length v2) (VectorToString v2)
 
-let v3 = v0-V([0.0..0.1..0.4])
-printfn "v0-V([0.0..0.1..0.4]) = v3 in R^%d =\n%s" (length v3) (VectorToString v3)
+let v3 = v0 - Vector( [0.0 .. 0.1 .. 0.4] )
+printfn "v0 - Vector( [0.0 .. 0.1 .. 0.4] ) = v3 in R^%d =\n%s" (length v3) (VectorToString v3)
 
 let v4 = 3.0*v3
 printfn "3.0*v3 = v4 in R^%d =\n%s" (length v4) (VectorToString v4)
@@ -22,8 +22,8 @@ let v6 = VectorNorm v3
 printfn "VectorNorm v3 = v6 = %f" v6
 
 let n = length v0
-let v7 = VectorGetSlice (v0, 0, n-1)
-printfn "VectorGetSlice (v0, 0, (length v0) -1) = v7 = %s" (VectorToString v7)
+let v7 = VectorGetSlice (v0, 0, n - 1)
+printfn "VectorGetSlice (v0, 0, (length v0) - 1) = v7 = %s" (VectorToString v7)
 
 let v8 = VectorGetSlice (v0, 0, 0)
 printfn "VectorGetSlice (v0, 0, 0) = v8 = %s" (VectorToString v8)
@@ -33,20 +33,20 @@ printfn "VectorGetSlice (v0, 1, 2) = v9 = %s" (VectorToString v9)
 
 printfn "VectorRandom 4 = %s"  (VectorToString (VectorRandom 4))
 
-let m0 = M([[1.0; 2.0];[3.0;4.0]])
+let m0 = Matrix( [[1.0; 2.0];[3.0;4.0]] )
 printfn "m0 in R^(%dx%d) =\n%s" (rows m0) (cols m0) (MatrixToString m0)
 
 let m1 = 3.0*m0
 printfn "3.0*m0 = m1 in R^(%dx%d) =\n%s" (rows m1) (cols m1) (MatrixToString m1)
 
-let m2 = -m0
-printfn "-m0 = m2 in R^(%dx%d) =\n%s" (rows m2) (cols m2) (MatrixToString m2)
+let m2 = - m0
+printfn " - m0 = m2 in R^(%dx%d) =\n%s" (rows m2) (cols m2) (MatrixToString m2)
 
-let m3 = m0+M([[5.0; 6.0];[7.0;8.0]])
-printfn "m0+M([[5.0; 6.0];[7.0;8.0]]) = m3 in R^(%dx%d) =\n%s" (rows m3) (cols m3) (MatrixToString m3)
+let m3 = m0 + Matrix( [[5.0; 6.0];[7.0;8.0]] )
+printfn "m0 + Matrix( [[5.0; 6.0];[7.0;8.0]] ) = m3 in R^(%dx%d) =\n%s" (rows m3) (cols m3) (MatrixToString m3)
 
-let m4 = m0-M([[5.0; 6.0];[7.0;8.0]])
-printfn "m0-M([[5.0; 6.0];[7.0;8.0]]) = m4 in R^(%dx%d) =\n%s" (rows m4) (cols m4) (MatrixToString m4)
+let m4 = m0 - Matrix( [[5.0; 6.0];[7.0;8.0]] )
+printfn "m0 - Matrix( [[5.0; 6.0];[7.0;8.0]] ) = m4 in R^(%dx%d) =\n%s" (rows m4) (cols m4) (MatrixToString m4)
 
 let m5 = transpose m0
 printfn "transpose m0 = m5 in R^(%dx%d) =\n%s" (rows m5) (cols m5) (MatrixToString m5)
@@ -56,7 +56,7 @@ printfn "m0*m5 = m6 in R^(%dx%d) =\n%s" (rows m6) (cols m6) (MatrixToString m6)
 
 let r = rows m0
 let c = cols m0
-printfn "MatrxGetSlice (m0, 0, r-1, 0, c-1) = %s" (MatrixToString (MatrixGetSlice (m0, 0, r-1, 0, c-1)))
+printfn "MatrxGetSlice (m0, 0, r - 1, 0, c - 1) = %s" (MatrixToString (MatrixGetSlice (m0, 0, r - 1, 0, c - 1)))
 
 printfn "MatrixGetSlice (m0, 0, 0, 0, 0)  = %s" (MatrixToString (MatrixGetSlice (m0, 0, 0, 0, 0)))
 
@@ -70,11 +70,11 @@ printfn "Minor (m0, 0, 0) = %s" (MatrixToString (Minor (m0, 0, 0)))
 
 printfn "Minor (m0, 1, 0) = %s" (MatrixToString (Minor (m0, 1, 0)))
 
-printfn "Minor (m0, -1, 0) = %s" (MatrixToString (Minor (m0, -1, 0)))
+printfn "Minor (m0, - 1, 0) = %s" (MatrixToString (Minor (m0, - 1, 0)))
 
 printfn "Minor (m0, 0, 1) = %s" (MatrixToString (Minor (m0, 0, 1)))
 
-printfn "Minor (m0, 0, -1) = %s" (MatrixToString (Minor (m0, 0, -1)))
+printfn "Minor (m0, 0, - 1) = %s" (MatrixToString (Minor (m0, 0, - 1)))
 
 printfn "Det m0 = %f" (Det m0)
 
@@ -82,8 +82,8 @@ let m7 = ColConcat (RowConcat (m0, m1), RowConcat (m2, m3))
 printfn "m7 = ColConcat (RowConcat (m0, m1), RowConcat (m2, m3)) = %s" (MatrixToString m7)
 printfn "Det m7 = %f" (Det m7)
 
-let v10 = Cramer (m0,V([1.0; 2.0]))
-printfn "v10 = Cramer (m0,V([1, 2])) = %s" (VectorToString v10)
+let v10 = Cramer (m0,Vector( [1.0; 2.0] ))
+printfn "v10 = Cramer (m0,Vector( [1, 2] )) = %s" (VectorToString v10)
 printfn "VectorToMatrix v10 = %s" (MatrixToString (VectorToMatrix v10))
 printfn "m0*v10 = %s" (MatrixToString (m0*v10))
 
