e622a33ded438eb47abe4e824b1b4c0561eb7eee
Jon Sporring <sporring@di.ku.dk>
2015-08-24 11:22:46 +0000

    minor

--- src/calcPi.fsx
+++ src/calcPi.fsx
@@ -2,15 +2,17 @@
  * http://math.stackexchange.com/questions/14815/why-does-this-converge-to-pi-4
  * https://en.wikipedia.org/wiki/Approximations_of_π
  *)
+
+open System.Threading
+
 let piArea n = 
    let mutable count = 0;
-   let m = int (sqrt (float n))
-   let m2 = (float m) ** 2.0
-   for i in 1 .. m do
-     for j in 1 .. m do
-       if (float i) ** 2.0+(float j) ** 2.0 <= m2 then
+   let n2 = (float n) ** 2.0
+   for i in 1 .. n do
+     for j in 1 .. n do
+       if (float i) ** 2.0+(float j) ** 2.0 <= n2 then
          count <- count + 1;
-   4.0 * (float count) / ((float m) * (float m))
+   4.0 * (float count) / n2
 
 let rec piGregoryLeibniz = function
    | 0 -> 4.0
@@ -33,24 +35,42 @@ let rec piBaileyBorweinPlouffe = function
      let m = 8.0 * (float k)
      (4.0 / (m + 1.0) - 2.0 / (m + 4.0) - 1.0 / (m + 5.0) - 1.0 / (m + 6.0)) / (pown 16.0 k) + piBaileyBorweinPlouffe (k-1)
 
+(* Won't work since this is a value and only executed once - at time of declaration
+let pressEnter =
+  printf "Press <enter>";
+  System.Console.ReadLine();
+*)
+   
+let pressEnter () =
+   printf "Press <enter>";
+   System.Console.ReadLine();
+
 let pi = 3.141592653589793238462643383279502884197169399375105820974944592307816406286;
 printfn "pi = %.18f..." pi
+
+pressEnter () |> ignore;
 printfn "piArea"
-for i in 0..10000 do
-    printf "%d: %.18f\r" i (piArea i)
+for i in 1..1000 do
+    printf "%d: %.18f\r" (i*i) (piArea i)
 printfn "";
 
+pressEnter () |> ignore;
 printfn "piGregoryLeibniz"
 for i in 0..10000 do
     printf "%d: %.18f\r" i (piGregoryLeibniz i)
+    Thread.Sleep 1
 printfn "";
 
+pressEnter () |> ignore;
 printfn "piNilakantha"
 for i in 0..10000 do
     printf "%d: %.18f\r" i (piNilakantha i)
+    Thread.Sleep 10
 printfn "";
 
+pressEnter () |> ignore;
 printfn "piBaileyBorweinPlouffe"
-for i in 0..10000 do
+for i in 0..10 do
     printf "%d: %.18f\r" i (piBaileyBorweinPlouffe i)
+    Thread.Sleep 1000
 printfn "";
