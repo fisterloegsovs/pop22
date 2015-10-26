46c80601d7f46581b927d8c9fdaa9b59a40b127e
Jon Sporring <sporring@di.ku.dk>
2015-10-26 11:59:55 +0000

    Today's lecture done

---
+++ fsharp/src/20151026_Modules/Vector.fs
@@ -0,0 +1,10 @@
+module Vector
+type Vector = V of float * float
+let ( ~-. ) (V(x1,y1)) = V(-x1,-y1)
+let ( +. ) (V(x1,y1)) (V(x2,y2)) = V(x1+x2,y1+y2)
+let ( -. ) (V(x1,y1)) (V(x2,y2)) = V(x1-x2,y1-y2)
+let ( *. ) a (V(x1,y1)) = V(a*x1,a*y1)
+let ( &. ) (V(x1,y1)) (V(x2,y2)) = x1*x2+y1*y2
+let norm (V(x,y)) = sqrt(x*x+y*y)
+let make (x,y) = V(x,y)
+let coord (V(x,y)) = (x,y)
