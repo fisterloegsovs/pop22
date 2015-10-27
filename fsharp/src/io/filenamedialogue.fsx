d3d601643a778c85680ed25719de41a310850fb2
Jon Sporring <sporring@di.ku.dk>
2015-10-27 08:42:25 +0000

    file io examples done

--- fsharp/src/io/filenamedialogue.fsx
+++ fsharp/src/io/filenamedialogue.fsx
@@ -1,3 +1,12 @@
+/// A demonstration of filename dialogue with user.
+///
+/// How to compile: <code>fsharpc --doc:filenamedialogue.xml filenamedialogue.fsx</code>
+///
+/// Author: Jon Sporring.
+/// Date: 2015/10/27
+
+/// <summary>Dialogue for asking the user to input the name of an existing file.</summary>
+/// <returns>A valid filename.</returns>
 let rec documentFileName () =
         System.Console.Write("Filename: ")
         let filename = System.Console.ReadLine()
@@ -6,6 +15,7 @@ let rec documentFileName () =
             documentFileName()
         else filename
 
+/// Print the content of the current directory, and ask the user to input an existing filename.
 let x = System.IO.Directory.GetFiles(".")
 printfn "Directory contains: %A" x
 documentFileName () |> ignore
--- fsharp/src/io/readFile.fsx
+++ fsharp/src/io/readFile.fsx
@@ -1,3 +1,12 @@
+/// A demonstration of reading a text file.
+///
+/// How to compile: <code>fsharpc --doc:readFile.xml readFile.fsx</code>
+///
+/// Author: Jon Sporring.
+/// Date: 2015/10/27
+
+/// <summary>Dialogue for asking the user to input the name of an existing file.</summary>
+/// <returns>A valid filename.</returns>
 let rec documentFileName () =
   System.Console.Write("Filename: ")
   let filename = System.Console.ReadLine()
@@ -6,13 +15,15 @@ let rec documentFileName () =
     documentFileName ()
   else filename
 
+/// <summary>Print the contents of a text file to screen.</summary>
+/// <param name="reader">An open stream.</param>
 let rec printFile (reader : System.IO.StreamReader) =
   if not(reader.EndOfStream) then
     let line = reader.ReadLine ()
     printfn "%s" line
     printFile reader
           
-
+/// Print the contents of the current directory and ask the user for a filename, and then print its contents to screen.
 let x = System.IO.Directory.GetFiles(".")
 printfn "Directory contains: %A" x
 
