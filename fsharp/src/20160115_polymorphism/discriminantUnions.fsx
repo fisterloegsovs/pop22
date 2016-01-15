ad80f4703c5b7012c4bb1359ce7294188ba1fd57
Jon Sporring <sporring@di.ku.dk>
2016-01-15 09:34:26 +0000

    minor

--- fsharp/src/20160115_polymorphism/discriminantUnions.fsx
+++ fsharp/src/20160115_polymorphism/discriminantUnions.fsx
@@ -1,6 +1,7 @@
 /// Discriminant unions are an example of polymorphism
 
-/// Example Symes et al., Expert F# 4.0, pp. 92: Transport is one of either Car, Bicyle, or Bus
+/// Example Symes et al., Expert F# 4.0, pp. 92: Transport is one of
+/// either Car, Bicyle, or Bus
 type Route = int
 type Make = string
 type Model = string
@@ -14,7 +15,8 @@ let ian = Car("BMW","360")
 let don = [Bicycle; Bus 8]
 let peter = [Car ("Ford","Fiesta"); Bicycle]
 
-/// We use pattern matching to define functions, but since it's a concrete type, then all possibilities must be matched
+/// We use pattern matching to define functions, but since it's a
+/// concrete type, then all possibilities must be matched
 let averageSpeed (tr : Transport) =
     match tr with
     | Car _ -> 35
