26beb89cb756136617ded0326177d8e524d85e89
Jon Sporring <sporring@di.ku.dk>
2016-01-20 14:59:22 +0000

    div

--- fsharp/src/20160119_ModelViewControl/ticker.fs
+++ fsharp/src/20160119_ModelViewControl/ticker.fs
@@ -8,9 +8,3 @@ let public generateTicker () =
   let mutable count = 0
   {new ITicker with
    member x.tick () = count <- count + 1; count}
-
-/// This function can only be called from this file and in this module
-let private f x = x**2.0
-
-/// This function can only be called from the same dll or exe
-let internal g x = 1/x
