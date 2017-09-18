20a0c007e6da673eaaf519928118b52e58c29b8d
Jon Sporring <sporring@di.ku.dk>
2017-09-18 21:36:13 +0000

    lecture ready

---
+++ lectures/3.2FunctionsNPrintf/commentExample.fsx
@@ -0,0 +1,30 @@
+/// The discriminant of a quadratic equation with parameters a, b, and c
+let discriminant a b c = b ** 2.0 - 4.0 * a * c
+
+/// <summary>Find x when 0 = ax^2+bx+c.</summary>
+/// <remarks>Negative discriminant are not checked.</remarks>
+/// <example>
+///   The following code:
+///   <code>
+///     let a = 1.0
+///     let b = 0.0
+///     let c = -1.0
+///     let xp = (solution a b c +1.0)
+///     printfn "0 = %.1fx^2 + %.1fx + %.1f => x_+ = %.1f" a b c xp
+///   </code>
+///   prints <c>0 = 1.0x^2 + 0.0x + -1.0 => x_+ = 0.7</c> to the console.
+/// </example>
+/// <param name="a">Quadratic coefficient.</param>
+/// <param name="b">Linear coefficient.</param>
+/// <param name="c">Constant coefficient.</param>
+/// <param name="sgn">+1 or -1 determines the solution.</param>
+/// <returns>The solution to x.</returns>
+let solution a b c sgn =
+  let d = discriminant a b c
+  (-b + sgn * sqrt d) / (2.0 * a)
+
+let a = 1.0
+let b = 0.0
+let c = -1.0
+let xp = (solution a b c +1.0)
+printfn "0 = %.1fx^2 + %.1fx + %.1f => x_+ = %.1f" a b c xp
