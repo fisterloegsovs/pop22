d3802562e13db0f80f5783cdd7f5f0694e68f7e9
Jon Sporring <sporring@di.ku.dk>
2019-08-16 13:02:41 +0000

    cleanup

--- fsharp/src/FsLab.Templates-basic/scatter.fsx
+++
@@ -1,7 +0,0 @@
-#load "packages/FsLab/FsLab.fsx"
-open System
-open XPlot.GoogleCharts
-let rnd = new System.Random()
-let data = [for i in 1 .. 15 -> (rnd.Next(10),rnd.Next(10))]
-//let data = [for i in 1 .. 15 -> (rnd.NextDouble (),rnd.NextDouble ())]
-Chart.Scatter data
