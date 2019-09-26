9db3e0d1b3c7205273a17bba50e5cb5c8acb6c84
Jon Sporring <sporring@di.ku.dk>
2019-09-26 06:37:21 +0000

    added examples from slides

---
+++ lectures/4.2StackHeapRefsHigherOrderFctsTracing/inc.fsx
@@ -0,0 +1,9 @@
+let incr = 
+  let mutable counter = 0
+  fun () ->                   
+    counter <- counter + 1
+    counter
+
+printfn "%d" (incr ())
+printfn "%d" (incr ())
+printfn "%d" (incr ())
