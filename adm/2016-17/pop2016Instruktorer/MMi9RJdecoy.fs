1ecffa3e173e0a82765accd40b6dc3e1fdd6f236
Jon Sporring <sporring@di.ku.dk>
2019-10-03 11:42:48 +0000

    old stuff found and added

---
+++ adm/2016-17/pop2016Instruktorer/MMi9RJdecoy.fs
@@ -0,0 +1,4 @@
+Dette er ikke den parser du leder efter.
+
+Lidt mere uddybende. Jeg har haft en del personlig ting at se til over den sidste uge, og dette er desværre gået en del ud over mit studie.
+Dette er selvfølgelig ikke en undskyldning for ikke at have lavet sin aflevering. Beklager.
