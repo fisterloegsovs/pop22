738c34f723989959bb1420562d4e02021a9f156b
Jon Sporring <sporring@di.ku.dk>
2020-01-15 12:17:51 +0000

    cleanup

---
+++ lectures/17GUI/triangle.fsx
@@ -0,0 +1,19 @@
+// Open often used libraries, beware of namespace polution!
+open System.Windows.Forms
+open System.Drawing
+
+// Prepare window form
+let win = new Form ()
+win.Size <- Size (320, 170)
+
+// Set paint call-back function
+let paint (e : PaintEventArgs) : unit =
+  printfn "redrawing"
+  let pen = new Pen (Color.Black)
+  let points =
+    [|Point (0,0); Point (10,170); Point (320,20); Point (0,0)|]
+  e.Graphics.DrawLines (pen, points)
+win.Paint.Add paint
+
+// Start the event-loop.
+Application.Run win
