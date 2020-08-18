748a4aed7bd4de7607b017a8464603796799810d
Jon Sporring <sporring@diku.dk>
2020-08-18 15:38:23 +0200

    old suff

---
+++ lectures/17GUI/transformWindows.fsx
@@ -0,0 +1,49 @@
+// Open often used libraries, beware of namespace polution!
+open System.Windows.Forms
+open System.Drawing
+
+///////////// WinForm specifics /////////////
+/// Setup a window form and return function to activate
+let view (sz : Size) (shapes : (Pen * (Point [])) list) : (unit -> unit) =
+  let win = new Form ()
+  win.ClientSize <- sz
+  let paint (e : PaintEventArgs) ((p, pts) : (Pen * (Point []))) : unit = 
+    e.Graphics.DrawLines (p, pts)
+  win.Paint.Add (fun e -> List.iter (paint e) shapes)
+  fun () -> Application.Run win // function as return value
+
+///////////// Model /////////////
+// A black triangle, using WinForm primitives for brevity
+let model () : Size * ((Pen * (Point [])) list) = 
+  /// Translate a primitive
+  let translate (d : Point) (arr : Point []) : Point [] =
+    let add (d : Point) (p : Point) : Point =
+      Point (d.X + p.X, d.Y + p.Y)
+    Array.map (add d) arr
+
+  /// Rotate a primitive
+  let rotate (theta : float) (arr : Point []) : Point [] =
+    let toInt = int << round
+    let rot (t : float) (p : Point) : Point =
+      let (x, y) = (float p.X, float p.Y)
+      let (a, b) = (x * cos t - y * sin t, x * sin t + y * cos t)
+      Point (toInt a, toInt b)
+    Array.map (rot theta) arr
+
+  let size = Size (400, 200)
+  let lines =
+    [|Point (0,0); Point (10,170); Point (320,20); Point (0,0)|]
+  let black = new Pen (Color.FromArgb (0, 0, 0))
+  let red = new Pen (Color.FromArgb (255, 0, 0))
+  let green = new Pen (Color.FromArgb (0, 255, 0))
+  let shapes =
+    [(black, lines);
+     (red, translate (Point (40, 30)) lines);
+     (green, rotate (1.0 *System.Math.PI / 180.0) lines)]
+  (size, shapes)
+
+///////////// Connection //////////////
+// Tie view and model together and enter main event loop
+let (size, shapes) = model ()
+let run = view size shapes
+run ()
