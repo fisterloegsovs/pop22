1c155bfeb5ceada2c3a13caeb363e19c18394305
Jon Sporring <sporring@diku.dk>
2021-01-06 23:50:48 +0100

    lecture ready

---
+++ lectures/15Chess/chessComputer/pieces.fs
@@ -0,0 +1,40 @@
+module Pieces
+open Chess
+
+/// <summary> A king is a chessPiece which moves 1 square in any direction. </summary>
+/// <param name = "col"> The color black or white </param>
+/// <returns> A king object. </returns>
+type king(col : Color) =
+  inherit chessPiece(col)
+  // king has runs of 1 in 8 directions: (N, NE, E, SE, S, SW, W, NW)
+  override this.candiateRelativeMoves =
+      [[(-1,0)];[(-1,1)];[(0,1)];[(1,1)];
+      [(1,0)];[(1,-1)];[(0,-1)];[(-1,-1)]]
+  override this.nameOfType = "king"
+  override this.copy () = king (col) :> chessPiece
+  override this.value = -1.0**(col |> Color2Int |> float)*1e4;
+
+/// <summary> A rook is a chessPiece which moves horisontally and vertically. </summary>
+/// <param name = "col"> The color black or white </param>
+/// <returns> A rook object. </returns>
+type rook(col : Color) =
+  inherit chessPiece(col)
+  // rook can move horisontally and vertically
+  // Make a list of relative coordinate lists. We consider the
+  // current position and try all combinations of relative moves
+  // (1,0); (2,0) ... (7,0); (-1,0); (-2,0); ...; (0,-7).
+  // Some will be out of board, but will be assumed removed as
+  // illegal moves.
+  // A list of functions for relative moves
+  let indToRel = [
+    fun elm -> (elm,0); // South by elm
+    fun elm -> (-elm,0); // North by elm
+    fun elm -> (0,elm); // West by elm
+    fun elm -> (0,-elm) // East by elm
+    ]
+  // For each function in indToRel, we calculate List.map f [1..7].
+  override this.candiateRelativeMoves =
+    List.map (fun e -> List.map e [1..7]) indToRel
+  override this.nameOfType = "rook"
+  override this.copy () = rook (col) :> chessPiece
+  override this.value = -1.0**(col |> Color2Int |> float)*10.0;
