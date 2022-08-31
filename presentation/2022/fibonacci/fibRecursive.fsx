b9b9a8850715d10bd3b6536824906845a2f1ed6f
Jon Sporring <sporring@di.ku.dk>
2022-08-31 12:04:12 +0200

    working on assignments

--- presentation/2022/fibonacci/fibRecursive.fsx
+++ presentation/2022/fibonacci/fibRecursive.fsx
@@ -1,10 +1,9 @@
 let rec fib n =
-  if n < 1 then
-    0
-  elif n = 1 then
-    1
-  else
-    fib (n - 1) + fib (n - 2)
+  match n with
+    0 | 1 ->
+      1
+    | _ ->
+      fib (n - 1) + fib (n - 2)
 
 for i = 0 to 45 do
   printfn "fib(%d) = %d" i (fib i)
