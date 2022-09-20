7a7bd35df6bb3e3d6fb60a6934e3c7745a740727
Jon Sporring <sporring@di.ku.dk>
2022-09-20 10:33:34 +0200

    simple similarity transform added

---
+++ exercises/assignments/forest/transform.fsx
@@ -0,0 +1,53 @@
+//
+// Simple turtle graphics
+//
+#r "nuget:diku.canvas, 1.0.1"
+open Canvas
+
+let translate theta delta lst =
+  [PenUp; Turn theta; Move delta; Turn -theta; PenDown] 
+  @ lst 
+  @ [PenUp; Turn theta; Move -delta; Turn -theta; PenDown] 
+
+let rotate theta lst =
+  Turn theta :: lst
+
+let rec scale s lst =
+  match lst with
+    [] -> []
+    | Move delta::rst -> Move (int (s*float delta))::scale s rst
+    | elm::rst -> elm::scale s rst
+
+let rec tree sz =
+   if sz < 5 then 
+     [Move sz; PenUp; Move (-sz); PenDown]
+   else 
+     [Move (sz/3); Turn -30] 
+     @ tree (sz*2/3) 
+     @ [Turn 30; Move (sz/6); Turn 25] 
+     @ tree (sz/2) 
+     @ [Turn -25; Move (sz/3); Turn 25] 
+     @ tree (sz/2) 
+     @ [Turn -25; Move (sz/6); PenUp; Move (-sz/3); Move (-sz/6); Move (-sz/3); Move (-sz/6); PenDown]
+
+let rnd = System.Random()
+
+let rec randomTree maxStep sz n =
+  match n with
+    0 -> []
+    | _ -> 
+      let theta = rnd.Next 360
+      let delta = rnd.Next maxStep
+      let s = 0.1+2.9*rnd.NextDouble ()
+      let phi = rnd.Next 20 - 10
+      let newTree = tree sz |> translate 180 100 |> rotate phi |> scale s |> translate theta delta
+      newTree @ randomTree maxStep sz (n-1)
+
+let w = 600
+let h = w
+let maxStep = w/4
+let sz = 100
+let nTrees = 20;
+let pic = randomTree maxStep sz nTrees
+
+do turtleDraw (w,h) "Forest" pic 
