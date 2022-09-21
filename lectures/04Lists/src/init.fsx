58285fd27f35d6dfe297f0dab0feae5970660882
Jon Sporring <sporring@di.ku.dk>
2022-09-21 22:59:49 +0200

    next weeks video lecture done

--- lectures/04Lists/src/init.fsx
+++ lectures/04Lists/src/init.fsx
@@ -1,5 +1,5 @@
-let rec init ((i,m): int*int) (f: int -> 'T) (lst: 'T list) : 'T list =
+let rec init (i: int) (m: int) (f: int -> 'T): 'T list =
   if i >= m then []
-  else (f i)::(init (i+1,m) f lst)
+  else (f i)::(init (i+1) m f)
 
-let lst = init (0,4) (fun i -> i*i) []
+let lst = init 0 4 (fun i -> i*i)
