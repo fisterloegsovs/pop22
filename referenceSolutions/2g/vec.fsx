6c28376747cecb9cacbbf7e0ec106386ff8700d0
Mads Obitsø <madsobitsoe@gmail.com>
2022-09-26 12:10:36 +0200

    Update ref-solution for 2g

--- referenceSolutions/2g/vec.fsx
+++ referenceSolutions/2g/vec.fsx
@@ -1,11 +1,7 @@
-(*
-   Initial reference solution by Jon Sporring,
-   made when the assignment was still in draft.
-   Your mileage may vary.
-*)
-#r "nuget:DIKU.Canvas"
-open System
+#r "nuget:DIKU.Canvas, 1.0"
 open Canvas
+
+type vec = float*float // A 2-dimensional vector
 
 /// <summary>Add two 2-dimensional vectors</summary>
 /// <param x1>x-coordinate of the first vector</param>
@@ -13,14 +9,19 @@ open Canvas
 /// <param x2>x-coordinate of the second vector</param>
 /// <param y2>y-coordinate of the second vector</param>
 /// <returns>The vector sum</returns>
-let add (x1,y1) (x2,y2) = (x1+x2,y1+y2)
+let add (v1: vec) (v2: vec) : vec =
+  let (x1,y1) = v1
+  let (x2,y2) = v2
+  (x1+x2,y1+y2)
 
 /// <summary>Multiplication of a vector with a scalar</summary>
 /// <param x>x-coordinate of the vector</param>
 /// <param y>y-coordinate of the vector</param>
 /// <param a>a multiplicative scalar</param>
 /// <returns>The scalar-vector product</returns>
-let mul (x,y) a = (a*x,a*y)
+let mul (v: vec) (a: float) : vec =
+  let (x,y) = v
+  (a*x,a*y)
 
 /// <summary>Dot two 2-dimensional vectors</summary>
 /// <param x1>x-coordinate of the first vector</param>
@@ -28,20 +29,27 @@ let mul (x,y) a = (a*x,a*y)
 /// <param x2>x-coordinate of the second vector</param>
 /// <param y2>y-coordinate of the second vector</param>
 /// <returns>The vector dot-product</returns>
-let dot (x1,y1) (x2,y2) = x1*x2+y1*y2
+let dot (v1: vec) (v2: vec) : float =
+  let (x1,y1) = v1
+  let (x2,y2) = v2
+  x1*x2+y1*y2
 
 /// <summary>Rotation of a vector around its tail</summary>
 /// <param x>x-coordinate of the vector</param>
 /// <param y>y-coordinate of the vector</param>
 /// <param a>an angle in the range 0..2 pi</param>
 /// <returns>The rotated vector</returns>
-let rot (x,y) a = (x*Math.Cos a - y*Math.Sin a, x*Math.Sin a + y*Math.Cos a)
+let rot (v: vec) (a: float) : vec =
+  let (x,y) = v
+  (x*cos a - y*sin a, x*sin a + y*cos a)
 
 /// <summary>Conversion of a vector wit float components to int</summary>
 /// <param x>x-coordinate of the vector</param>
 /// <param y>y-coordinate of the vector</param>
 /// <returns>The vector casted to int</returns>
-let toInt (x,y) = (int x, int y)
+let toInt (v: vec) : int*int =
+  let (x,y) = v
+  (int x, int y)
 
 /// <summary>Update the canvas with a vector drawn as a line from p to p+v</summary>
 /// <param C>a canvas</param>
@@ -49,7 +57,8 @@ let toInt (x,y) = (int x, int y)
 /// <param v>a vector</param>
 /// <param p>a vector giving the position of the tail to be drawn</param>
 /// <returns>The canvas is updated as a side-effect</returns>
-let setVector C col v p = setLine C col (toInt p) (toInt (add v p))
+let setVector (C: canvas) (col: color) (v: vec) (p: vec) : unit = 
+  setLine C col (toInt p) (toInt (add v p))
 
 
 ( // Testing the basic operations
@@ -60,7 +69,7 @@ let setVector C col v p = setLine C col (toInt p) (toInt (add v p))
   printfn "v = %A, w = %A, a = %A" v w a
   printfn "v + w = %A" (add v w)
   printfn "av = %A" (mul v a)
-  printfn "R (1,0) = %A" (rot (1,0) (Math.PI/2.0))
+  printfn "R (1,0) = %A" (rot (1.0,0.0) (System.Math.PI/2.0))
 )
 
 type state = float // The spoke-angle offset will be communicated between draw and react
@@ -71,7 +80,7 @@ type state = float // The spoke-angle offset will be communicated between draw a
   /// <param h>the height of the resulting canvas</param>
   /// <param s>the angular offset of all spokesr</param>
   /// <returns>a canvas with spokes</returns>
-  let draw w h (s:state) =
+  let draw (w: int) (h: int) (s:state) =
     let rec fan C col u a n =
       match n with 
         0 -> ()
@@ -80,7 +89,7 @@ type state = float // The spoke-angle offset will be communicated between draw a
           setVector C col v (300.0,300.0)
           fan C col u a (n-1)
     let n = 36
-    let da = 2.0*Math.PI/float n
+    let da = 2.0*System.Math.PI/float n
     let C = create w h
     let u = (0.0,float (h/2))
     fan C black u da n
