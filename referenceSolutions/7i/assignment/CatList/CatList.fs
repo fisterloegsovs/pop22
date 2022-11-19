077a438759bc324879e5f2da16da3e1132777504
Mads Obitsø <madsobitsoe@gmail.com>
2022-11-19 12:11:13 +0100

    Add reference solution for CatList part of assignment

---
+++ referenceSolutions/7i/assignment/CatList/CatList.fs
@@ -0,0 +1,77 @@
+module CatList
+open DiffList
+
+type 'a catlist =
+    | Empty
+    | Single of 'a
+    | Append of 'a catlist * 'a catlist
+
+
+let nil = Empty
+let single (elm:'a) : 'a catlist =
+    Single elm
+
+let append (xs : 'a catlist) (ys : 'a catlist) : 'a catlist =
+    Append (xs, ys)
+    
+let cons (elm : 'a) (xs : 'a catlist) : 'a catlist =
+    append (single elm) xs
+
+let snoc (xs : 'a catlist) (elm : 'a) : 'a catlist =
+    append xs (single elm)
+
+let fold (cf:('a -> 'a -> 'a),(e:'a)) (f:('b->'a)) (xs:'b catlist) : 'a =
+    let rec g xs =
+        match xs with
+        | Empty -> e
+        | Single x -> f x        
+        | Append (ys,zs) -> cf (g ys) (g zs)
+    g xs
+
+// length is not required for assignment but is a nifty helper function
+let length (xs : 'a catlist) = fold ((+), 0) (fun _ -> 1) xs
+
+
+let fromCatList (xs : 'a catlist) : 'a list =
+    DiffList.fromDiffList (fold (DiffList.append, DiffList.nil) DiffList.single xs)
+
+let toCatList (xs : 'a list) : 'a catlist =
+    List.foldBack cons xs nil
+
+let item (i:int) (xs : 'a catlist) : 'a =
+    let rec f i' xs =
+        match xs with
+        | Empty -> failwith "Precondition not satisfied, catlist was empty"
+        | Single x when i' = 0 -> x
+        | Append (ys, zs) ->
+            let ylen = length ys
+            if ylen > i' then f i' ys
+            else f (i' - ylen) zs
+
+    if length xs < i then failwith <| sprintf "Precondition violated: catlist has less than %d elements" i
+    else
+        f i xs
+
+let insert (i:int) (elm:'a) (xs:'a catlist) : 'a catlist =
+    let rec f i' xs =
+        match xs with
+            | Empty when i' = 0 -> single elm
+            | Single x when i' = 0 -> append xs (single elm)
+            | Append (ys,zs) ->
+                let ylen = length ys
+                if ylen > i' then append (f i' ys) zs
+                else append ys (f (i' - ylen) zs)
+            
+    f i xs
+
+let delete (i:int) (xs:'a catlist) : 'a catlist =
+    let rec f i' xs =
+        match xs with
+            | Empty -> failwith "Precondition violated"
+            | Single _ when i' = 0 -> nil
+            | Append (ys,zs) ->
+                let ylen = length ys
+                if ylen > i' then append (f i' ys) zs
+                else append ys (f (i' - ylen) zs)
+    f i xs
+
